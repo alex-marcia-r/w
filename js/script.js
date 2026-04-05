09099bfc7f12906c2d68ebf3d329ee50b10b093c
Reynaldo Alexander Marcia Ramirez <reynaldomr2006@gmail.com>
2026-04-05 16:06:54 -0600

    encabezado

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const header = document.querySelector('header');
 
     window.addEventListener('scroll', ()=> {
-    header.classList.toggle('sticky', this.window,scrollY > 100);
+    header.classList.toggle('sticky', this.window,scrollY > 70);
     })
 
